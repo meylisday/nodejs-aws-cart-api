2f3fb151c26af529573f74b51631eca955197189
meylisday <victory.sett@gmail.com>
2023-12-31 18:38:53 +0100

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,41 @@
-module.exports = (options, webpack) => {
-    const lazyImports = [
-      '@nestjs/microservices/microservices-module',
-      '@nestjs/websockets/socket-module',
-    ];
+const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
 
-    return {
-      ...options,
-      externals: [],
-      plugins: [
-        ...options.plugins,
-        new webpack.IgnorePlugin({
-          checkResource(resource) {
-            if (lazyImports.includes(resource)) {
-              try {
-                require.resolve(resource);
-              } catch (err) {
-                return true;
-              }
+module.exports = function (options) {
+  const lazyImports = [
+    '@nestjs/microservices/microservices-module',
+    '@nestjs/websockets/socket-module',
+  ];
+
+  return {
+    ...options,
+    plugins: [
+      ...options.plugins,
+      new webpack.IgnorePlugin({
+        checkResource(resource) {
+          if (lazyImports.includes(resource)) {
+            try {
+              require.resolve(resource);
+            } catch (err) {
+              return true;
             }
-            return false;
+          }
+          return false;
+        },
+      }),
+    ],
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            keep_classnames: true,
           },
         }),
       ],
-    };
+    },
+    output: {
+      ...options.output,
+      libraryTarget: 'commonjs2',
+    },
   };
+};
